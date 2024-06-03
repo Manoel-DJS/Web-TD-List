54e66746542cdea0a87bdd3449456f8902d79528
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2024-06-03 20:26:12 -0300

    First Commit

---
+++ components/Task.js
@@ -0,0 +1,6 @@
+const Task = () => {
+    return(
+        <h1>My Task</h1>
+    )
+}
+export default Task;
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Task from "@/components/Task";
+import { useEffect, useState } from "react";
+
+const App = () => {
+
+    return(
+        <>
+        <div id="container" style={{backgroundColor: "black"}}>
+
+        </div>
+        <Task/>
+        </>
+    )
+}
+
+export default App;
